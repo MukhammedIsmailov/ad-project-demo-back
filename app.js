52a78836d94dd7958255f2842cf136f603055293
mukhammed.ismailov <mukhammed.ismailov.home@gmail.com>
2019-10-19 18:33:51 +0300

    small fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { configureWebPush } = require('./helpers/webPush');
 
 const app = express();
 
-app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.json());
 
 app.post('/subscribe', (req, res) => {
 
@@ -14,15 +14,13 @@ app.post('/subscribe', (req, res) => {
 
     res.status(201).json({});
 
-    const payload = JSON.stringify({ title: "Push Test" });
+    const payload = JSON.stringify({ title: "Push Test", body: "Hello World!", icon: 'https://img8.apk.tools/150/b/d/1/com.abdallah_abdelazim.helloworld.png' });
 
     const webPush = configureWebPush();
 
     webPush
         .sendNotification(subscription, payload)
         .catch(err => console.error(err));
-
-    res.send(config.VAOIDPrivateKey);
 });
 
 app.listen(port, () => {
--- helpers/webPush.js
+++ helpers/webPush.js
@@ -2,11 +2,11 @@ const webPush = require('web-push');
 const { publicVapidKey, privateVapidKey, email } = require('../config/config');
 
 exports.configureWebPush = () => {
-    webPush.setVapidDetails({
+    webPush.setVapidDetails(
+        `mailto:${email}`,
         publicVapidKey,
-        privateVapidKey,
-        email
-    });
+        privateVapidKey
+    );
 
     return webPush;
 };
